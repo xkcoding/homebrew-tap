ccece75bdd4d260dfdfe4fe10db7c88e1045c0d0
Yangkai.Shen <237497819@qq.com>
2026-03-12 09:42:15 +0000

    feat: update GhostClip to v0.1.0

--- Casks/ghostclip.rb
+++ Casks/ghostclip.rb
@@ -1,8 +1,8 @@
 cask "ghostclip" do
   version "0.1.0"
-  sha256 "6f2a204bebf5fcc598b3a3cd09730232e3a4b5492822b31c5efa3669005f5e34"
+  sha256 "ba48d3a5553e09290f03a271762d0c5666ed3c0420572e2759a3f184c241826d"
 
-  url "https://github.com/xkcoding/GhostClip/releases/download/v#{version}/GhostClip_0.1.0_aarch64.dmg"
+  url "https://github.com/xkcoding/GhostClip/releases/download/v#{version}/GhostClip_0.1.0_universal.dmg"
   name "GhostClip"
   desc "Android <-> Mac clipboard sync tool"
   homepage "https://github.com/xkcoding/GhostClip"
