aef0a3b1037850a4004b0c81aecc602944a68501
Yangkai.Shen <237497819@qq.com>
2026-03-12 08:00:26 +0000

    feat: update GhostClip to v0.1.0

--- Casks/ghostclip.rb
+++ Casks/ghostclip.rb
@@ -1,6 +1,6 @@
 cask "ghostclip" do
   version "0.1.0"
-  sha256 "618c7bcd571a6f187ed96415f910688c9b1a08566c0fb3cb4d0fd6bbb3645e57"
+  sha256 "6f2a204bebf5fcc598b3a3cd09730232e3a4b5492822b31c5efa3669005f5e34"
 
   url "https://github.com/xkcoding/GhostClip/releases/download/v#{version}/GhostClip_0.1.0_aarch64.dmg"
   name "GhostClip"
